c2388cb041353bdb413f1e0fa5d2ff7435378330
Cerberuser <k.bagrov@g.nsu.ru>
2019-09-18 10:01:40 +0300

    Using the batch_run fork instead of trybuild

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,6 +1,7 @@
-use trybuild::TestCases;
+use batch_run::Batch;
 
 fn main() {
-    let t = TestCases::new();
-    t.pass(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/entries/**/main.rs");
+    let t = Batch::new();
+    t.run_pass(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/entries/**/main.rs");
+    t.run();
 }
