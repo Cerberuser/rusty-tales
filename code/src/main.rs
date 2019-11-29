855d650c121a9b7e40def29127a05b8cc8288ca2
Cerberuser <k.bagrov@g.nsu.ru>
2019-11-29 11:17:21 +0300

    Batch_run is using features list

--- code/src/main.rs
+++ code/src/main.rs
@@ -2,9 +2,10 @@ use batch_run::Batch;
 
 fn main() {
     let list: Vec<&str> = include!(concat!(env!("OUT_DIR"), "/list.rs"));
-    println!("{:?}", list);
-
     let t = Batch::new();
-    t.run_pass("entries/**/main.rs");
+    for item in list {
+        // A glob pattern here is a hack to force batch_run ignore non-existent paths
+        t.run_pass(format!("entries/{}/main*.rs", item));
+    }
     t.run().unwrap();
 }
